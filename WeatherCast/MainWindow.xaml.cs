0f1e749fdc8f05a0b52e31865344a13d3141a87b
Artem <artemef1996@gmail.com>
2022-04-06 20:29:25 +0300

    Parsing the json information andshowing current temperature

--- WeatherCast/MainWindow.xaml.cs
+++ WeatherCast/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WeatherCast
 {
@@ -23,6 +13,120 @@ namespace WeatherCast
         public MainWindow()
         {
             InitializeComponent();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            APIControl control = new APIControl();
+
+            WeatherResponse response = control.GetResponse();
+
+            MessageBox.Show($"Температура : {response.Main.Temp} \t Ощущается как : {response.Main.Feels_Like}");
         }
     }
+
+    public class APIControl
+    {
+        private string url = "http://api.openweathermap.org/data/2.5/weather?q=Ivanovo&units=metric&appid=8b946297edc5dc36bd60f3acab86dc68";
+
+        private HttpWebRequest httpWebRequest { get; set; }
+
+        private HttpWebResponse httpWebResponse { get; set; }
+
+        public string Response { get; set; }
+
+        private void WebRequestResponse()
+        {
+            httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+        }
+
+        public WeatherResponse GetResponse()
+        {
+            WebRequestResponse();
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                Response = streamReader.ReadToEnd();
+            }
+
+            WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(Response);
+        
+            return weatherResponse; 
+        }
+
+    }
+
+    public class WeatherResponse
+    {
+        public string Name { get; set; }
+
+        public TemperatureInfo Main { get; set; }
+    }
+
+    public class TemperatureInfo
+    {
+        public float Temp { get; set; }
+
+        public float Feels_Like { get; set; }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+/*
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Ivanovo&units=metric&appid=8b946297edc5dc36bd60f3acab86dc68";
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest) WebRequest.Create(url);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse) httpWebRequest.GetResponse();
+
+            string response;
+            
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+                Console.WriteLine();
+
+            WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
+
+            Console.WriteLine($"Температура сейчас: {weatherResponse.Main.Temp}");
+            Console.WriteLine($"Ощущается как: {weatherResponse.Main.Feels_Like}");
+
+            Console.ReadLine();
+        }
+    }
+
+    public class WeatherResponse
+    {
+        public string Name { get; set; }
+
+        public TemperatureInfo Main { get; set; }
+    }
+
+    public class TemperatureInfo
+    {
+        public float Temp { get; set; }
+
+        public float Feels_Like { get; set; }
+    }
+
+*/
