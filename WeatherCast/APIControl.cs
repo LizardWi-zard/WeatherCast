41bfacc5ee0e264f54085f81d0fc5ee9a881b566
Artem <artemef1996@gmail.com>
2022-04-09 21:09:34 +0300

    ApiControl class created as another file

---
+++ WeatherCast/APIControl.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherCast
+{
+    public class APIControl
+    {
+        private string url = "http://api.openweathermap.org/data/2.5/weather?q=Ivanovo&units=metric&appid=8b946297edc5dc36bd60f3acab86dc68";
+
+        private HttpWebRequest httpWebRequest { get; set; }
+
+        private HttpWebResponse httpWebResponse { get; set; }
+
+        public string Response { get; set; }
+
+        private void WebRequestResponse()
+        {
+            httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+        }
+
+        public WeatherResponse GetResponse()
+        {
+            WebRequestResponse();
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                Response = streamReader.ReadToEnd();
+            }
+
+            WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(Response);
+
+            return weatherResponse;
+        }
+
+    }
+
+    public class WeatherResponse
+    {
+        public string Name { get; set; }
+
+        public TemperatureInfo Main { get; set; }
+    }
+
+    public class TemperatureInfo
+    {
+        public float Temp { get; set; }
+
+        public float Feels_Like { get; set; }
+    }
+}
