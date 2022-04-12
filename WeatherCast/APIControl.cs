743ab18c844fe05224a7791fc934d2e4efa195b6
Artem <artemef1996@gmail.com>
2022-04-12 19:37:30 +0300

    APIControl class handles an exeption where link is wrong or null

--- WeatherCast/APIControl.cs
+++ WeatherCast/APIControl.cs
@@ -11,35 +11,56 @@ namespace WeatherCast
 {
     public class APIControl
     {
-        private string url = "http://api.openweathermap.org/data/2.5/weather?q=Ivanovo&units=metric&appid=8b946297edc5dc36bd60f3acab86dc68";
+        public HttpWebRequest httpWebRequest { get; set; }
 
-        private HttpWebRequest httpWebRequest { get; set; }
+        public HttpWebResponse httpWebResponse { get; set; }
 
-        private HttpWebResponse httpWebResponse { get; set; }
+        string response;
 
-        public string Response { get; set; }
+        string url;
 
-        private void WebRequestResponse()
+        public void CreateAPIurl(string str)
         {
-            httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-
-            httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + str + "&units=metric&appid=8b946297edc5dc36bd60f3acab86dc68";
         }
 
         public WeatherResponse GetResponse()
         {
-            WebRequestResponse();
-
-            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            if (WebRequestResponse() != null)
             {
-                Response = streamReader.ReadToEnd();
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    response = streamReader.ReadToEnd();
+                }
+
+                Console.WriteLine();
+
+                WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
+
+                return weatherResponse;
             }
 
-            WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(Response);
-
-            return weatherResponse;
+            return null;
         }
 
+        private HttpWebResponse WebRequestResponse()
+        {
+            httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+
+
+            try
+            {
+                httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return httpWebResponse;
+
+        }
     }
 
     public class WeatherResponse
@@ -47,6 +68,15 @@ namespace WeatherCast
         public string Name { get; set; }
 
         public TemperatureInfo Main { get; set; }
+
+        public WeatherInfo[] Weather { get; set; }
+    }
+
+    public class WeatherInfo
+    {
+        public string Main { get; set; }
+
+        public string Description { get; set; }
     }
 
     public class TemperatureInfo
