0b7c597494cb1fd6931eb28c9aa21e3088bea6af
R7G3 <tulahas@yandex.ru>
2022-07-21 01:24:06 +0300

    remove unused code

--- WeatherCast.Tests/TestHelper.cs
+++ WeatherCast.Tests/TestHelper.cs
@@ -2,13 +2,6 @@
 {
     internal static class TestHelper
     {
-        internal static Task Wait(TimeSpan delay)
-        {
-            Task.Run(async () => await Task.Delay(delay));
-
-            return Task.FromResult(0);
-        }
-
         internal static async Task WaitAsync(TimeSpan delay)
         {
             await Task.Run(async () => await Task.Delay(delay));
